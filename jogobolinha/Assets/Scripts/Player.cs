8390a141d7c9743f58be5d20ca56dce76b83c373
Anna Gabrielle Inacio Gomes <20221170150007@ifrn.local>
2023-10-11 10:10:44 -0300

    Movebola

--- jogobolinha/Assets/Scripts/Player.cs
+++ jogobolinha/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,32 @@ public class Player : MonoBehaviour
 {
     public int velocidade = 10;
     private Rigidbody rb;
+    public int forcaPulo = 7;
+
+    public bool noChao = false;
+    
     // Start is called before the first frame update
     void Start()
     {
         TryGetComponent(out rb);
 
     }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        noChao = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
+        
+        if (noChao && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
+            float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         
         rb.AddForce(new Vector3(h, 0, v) * velocidade * Time.deltaTime, ForceMode.Impulse);
